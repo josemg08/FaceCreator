d1cdf0444022982c3480d0862a22544956092196
josemg08 <josemg08@gmail.com>
2016-09-14 17:36:45 -0300

    Add physics library

--- app/src/main/java/sandbox/josegonzalez/fragmentpaplet/ItemDetailFragment.java
+++ app/src/main/java/sandbox/josegonzalez/fragmentpaplet/ItemDetailFragment.java
@@ -1,6 +1,7 @@
 package sandbox.josegonzalez.fragmentpaplet;
 
 import processing.core.PApplet;
+import shiffman.box2d.Box2DProcessing;
 
 /**
  * A fragment representing a single Item detail screen.
@@ -10,18 +11,27 @@ import processing.core.PApplet;
  */
 public class ItemDetailFragment extends PApplet {
 
+    private Box2DProcessing box2d;
+
     @Override
     public void setup() {
         fill(255,0,0);
+        box2d = new Box2DProcessing(this);
+        box2d.createWorld();
+        box2d.listenForCollisions();
     }
 
     @Override
-    public void draw() {
-        ellipse(30,30,30,30);
-    }
+    public void draw() {}
 
     @Override
     public void settings() {
         size(1000, 1000);
     }
+
+    @Override
+    public void mousePressed(){
+        ellipse(mouseX, mouseY, 30, 30);
+    }
+
 }
